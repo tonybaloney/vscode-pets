fc8f19fda35f81053eaadb019e82357de5a5ec62
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-05-28 07:11:24 +0000

    Fix merge issue.

--- src/extension/test/suite/extension.test.js
+++ src/extension/test/suite/extension.test.js
@@ -1,11 +1,13 @@
-import * as assert from 'assert';
+'use strict';
+exports.__esModule = true;
+var assert = require('assert');
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+var vscode = require('vscode');
 // import * as myExtension from '../../extension';
-suite('Extension Test Suite', () => {
+suite('Extension Test Suite', function () {
     vscode.window.showInformationMessage('Start all tests.');
-    test('Sample test', () => {
+    test('Sample test', function () {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
