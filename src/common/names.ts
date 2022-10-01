8337509a1da7a448a5e2402ef93af879611963eb
BananaToffee <46435382+jasminejell@users.noreply.github.com>
2022-10-01 12:07:12 +1000

    Update names.ts

--- src/common/names.ts
+++ src/common/names.ts
@@ -324,5 +324,5 @@ export const COCKATIEL_NAMES: Map<number, string> = new Map<number, string>([
     [9, 'Jack Sparrow'],
     [10, 'Godfeather'],
     [11, 'Mickey'],
-    [12, 'Kanye nest'],
+    [12, 'Kanye Nest'],
 ]);
