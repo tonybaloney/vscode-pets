f59a0d4b0090f242118171fe13dc1340d0002ec0
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-10-10 18:47:13 +1100

    Setup basic strings for extension API

--- src/common/localize.ts
+++ src/common/localize.ts
@@ -1,7 +1,29 @@
 import * as vscode from 'vscode';
 
-export const { init, localize } = require("vscode-nls-i18n");
+import * as i18n from 'vscode-nls-i18n';
 
 export function activate(context: vscode.ExtensionContext) {
-  init(context.extensionPath);
+    i18n.init(context.extensionPath);
+}
+
+export function localize(key: string, params: any): any {
+    return i18n.localize(`vscodePets.${key}`, params);
+}
+
+export class TranslatedQuickPickItem<T> implements vscode.QuickPickItem {
+    label: string;
+    value: T;
+
+    constructor(label: string, value: T) {
+        this.label = label;
+        this.value = value;
+    }
+}
+
+export function stringListAsQuickPickItemList<T>(
+    collection: Array<T>,
+): TranslatedQuickPickItem<T>[] {
+    return collection.map<TranslatedQuickPickItem<T>>((el) => {
+        return { label: localize(String(el), el), value: el };
+    });
 }
