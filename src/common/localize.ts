18f10a22a41c6ab7d2380d625b3ae0bd856eed32
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-10-28 13:03:01 +1100

    Use l10n

--- src/common/localize.ts
+++ src/common/localize.ts
@@ -1,16 +1,5 @@
-import * as nls from 'vscode-nls';
 import * as vscode from 'vscode';
 
-const localizeNls = nls.config()();
-
-// eslint-disable-next-line no-unused-vars
-export function activate(context: vscode.ExtensionContext) {
-    // Does nothing, used by activated extension.
-}
-
-export function localize(key: string, params: any): any {
-    return localizeNls(`vscodePets.${key}`, params);
-}
 
 export class TranslatedQuickPickItem<T> implements vscode.QuickPickItem {
     label: string;
@@ -26,6 +15,6 @@ export function stringListAsQuickPickItemList<T>(
     collection: Array<T>,
 ): TranslatedQuickPickItem<T>[] {
     return collection.map<TranslatedQuickPickItem<T>>((el) => {
-        return { label: localize(String(el), el), value: el };
+        return { label: vscode.l10n.t(String(el)), value: el };
     });
 }
