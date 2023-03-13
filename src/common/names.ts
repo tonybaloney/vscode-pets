62c83c84b1a794208d903619c0c218b31bfeeb2e
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-03-13 18:21:54 +1100

    Update names.ts

--- src/common/names.ts
+++ src/common/names.ts
@@ -33,7 +33,7 @@ export function randomName(type: PetType): string {
                 [PetType.rocky]: ROCKY_NAMES,
                 [PetType.cockatiel]: COCKATIEL_NAMES,
                 [PetType.rat]: RAT_NAMES,
-                [PetType.turtle]: TURTLE_NAMES
+                [PetType.turtle]: TURTLE_NAMES,
             } as Record<PetType, ReadonlyArray<string>>
         )[type] ?? CAT_NAMES;
 
