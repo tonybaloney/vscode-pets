d63d084c448f2ee43e0cc06b50bbc6e0664714b0
WoofWoof0 <135199332+WoofWoof0@users.noreply.github.com>
2024-06-11 21:58:11 -0600

    added snail into names.ts and types.ts.

--- src/common/names.ts
+++ src/common/names.ts
@@ -8,6 +8,7 @@ import { FOX_NAMES } from '../panel/pets/fox';
 import { MOD_NAMES } from '../panel/pets/mod';
 import { ROCKY_NAMES } from '../panel/pets/rocky';
 import { DUCK_NAMES } from '../panel/pets/rubberduck';
+import { SNAIL_NAMES } from '../panel/pets/snail';
 import { SNAKE_NAMES } from '../panel/pets/snake';
 import { TOTORO_NAMES } from '../panel/pets/totoro';
 import { ZAPPY_NAMES } from '../panel/pets/zappy';
@@ -28,6 +29,7 @@ export function randomName(type: PetType): string {
                 [PetType.clippy]: CLIPPY_NAMES,
                 [PetType.mod]: MOD_NAMES,
                 [PetType.totoro]: TOTORO_NAMES,
+                [PetType.snail]: SNAIL_NAMES,
                 [PetType.snake]: SNAKE_NAMES,
                 [PetType.rubberduck]: DUCK_NAMES,
                 [PetType.zappy]: ZAPPY_NAMES,
